2d4a290839a002b5a720ce0f1640c2e3a90c608a
joana <joanakatherinemeza@gmail.com>
2024-06-28 17:00:28 -0300

    mi primera subida

---
+++ Ejemplo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ejemplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Joana meza futura creadora mundial de app");
+            Console.ReadKey();
+        }
+    }
+}
